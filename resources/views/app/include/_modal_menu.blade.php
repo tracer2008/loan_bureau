60d1d2e9d47dd0e47b86f4122bb7ae4b78d3ae37
tracer2008 <mihail@webstyle.top>
2026-03-13 16:31:01 +0300

    city fix

--- resources/views/app/include/_modal_menu.blade.php
+++ resources/views/app/include/_modal_menu.blade.php
@@ -15,12 +15,14 @@
                 <li class="menu_item"><a href="#kak_oplatit_zaim" class="menu_link">Как оплатить займ</a></li>
                 <li class="menu_item"><a href="#usloviya_zaimov" class="menu_link">Условия займов</a></li>
                 <li class="menu_item"><a href="#kak_prodlit_zaim" class="menu_link">Как продлить займ</a></li>
-                <li class="menu_item">
-                    <span class="city_wrap p_relative row">
-                        <img src="{{asset('images/icons/map-marker.svg')}}" alt="" class="city_map_icon">
-                        <span class="city_name _city_name">Белгород</span>
-                    </span>
-                </li>
+                @if($subdomain != null)
+                    <li class="menu_item">
+                        <span class="city_wrap p_relative row">
+                            <img src="{{asset('images/icons/map-marker.svg')}}" alt="" class="city_map_icon">
+                            <span class="city_name _city_name">Белгород</span>
+                        </span>
+                    </li>
+                @endif
             </ul>
 
         </div>
